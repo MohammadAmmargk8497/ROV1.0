acc750309c02ac13d88b50804af45ebdc87089ef
Mohammad Ammar <ammar@Mohammads-MacBook-Air.local>
2024-05-02 23:17:13 +0530

    Future directions

--- Control.py
+++ Control.py
@@ -1,21 +1,20 @@
-import sys
 import pigpio
 import numpy as np
 import pygame
+from PID import PID
 import time
 import threading
 import queue
 import socket
 import json
 
-
-# Following is the controller class responsible for reading joystick data fro the serial bus using pygame.
+## Following is the controller class responsible for reading joystick data fro the serial bus using pygame.
 class Controller(object):
     STICK_DEADBAND = .05
 
     def __init__(self, axis_map):
         self.joystick = None
-        self.axis_map = axis_map  # different controllers have different key maps
+        self.axis_map = axis_map #different controllers have different key maps
 
     def update(self):
         pygame.event.pump()
@@ -32,16 +31,15 @@ class Controller(object):
     def getYaw(self):
         return self._getAxis(3)
 
-    def _getAxis(self, k):  # passed the key maps of the controller
-        j = self.axis_map[k]  # extract the value of the key from the tuple passed
-        val = self.joystick.get_axis(abs(j))
+    def _getAxis(self, k): #passed the key maps of the controller
+        j = self.axis_map[k] # extract the value of the key from the tuple passed
+        val = self.joystick.get_axis(abs(j)) 
         if abs(val) < Controller.STICK_DEADBAND:
             val = 0
         return (-1 if j < 0 else +1) * val
 
-
 #______________________________________________________________________GameController____________________________________________________________
-
+    
 class _GameController(Controller):
 
     def __init__(self, axis_map, button_id):
@@ -62,9 +60,8 @@ controllers = {
     'Logitech Logitech Extreme 3D':
         _GameController((-3, 0, -1, 2), 0),
 
-    #Add your own controller here:
+        #Add your own controller here:
 }
-
 
 #______________________________________________________________________________Control-Generator_________________________________________________________________________
 class Control(Controller):
@@ -74,13 +71,13 @@ class Control(Controller):
         self.THRUSTER_2 = THRUSTER_2
         self.THRUSTER_3 = THRUSTER_3
         self.THRUSTER_4 = THRUSTER_4
-        self.control_queue = queue.Queue()              # Thread safe Queue for Producer-Consumer Thread Concurrency
+        self.control_queue = queue.Queue() #Thread safe Queue for Producer-Consumer Thread Concurrency
 
         thruster_pins = [THRUSTER_1, THRUSTER_2, THRUSTER_3, THRUSTER_4]
-        thvalue = [1500, 1500, 1500, 1500]
+        thvalue = [1500, 1500, 1500, 1500] ##!!
         pi = pigpio.pi()
         for item in thruster_pins:
-            pi.set_servo_pulsewidth(item, 1500)     # Arming the thrusters for the first time
+            pi.set_servo_pulsewidth(item, 1500) #Arming the thrusters for the first time
 
     def get_controller(self):
 
@@ -94,32 +91,24 @@ class Control(Controller):
         controller_name = joystick.get_name()
         if controller_name not in controllers.keys():
             print('Unrecognized controller: %s' % controller_name)
-            sys.exit(1)
+            exit(1)
         controller = controllers[controller_name]
         controller.joystick = joystick
 
         return controller
 
-    def map_values(self, value):
-        """
-        Simple mapping function (Linear mapping for forward controls)
-        """
+    def map_values(self, value):  #Simple mapping function (Linear mapping for forward controls)
         if value < -1 or value > 1:
             return None
-        return int(1500 + (value * 300))
+        return int(1500 + (value * 300)) 
 
-    def map_values_depth(self, value):
-        """
-        Simple Linear Mapping for Depth ( for manual depth control when no PID is used)
-        """
+    def map_values_depth(self, value): #Simple Linear Mapping for Depth ( for manual depth control when no PID is used)
         if value < -1 or value > 1:
             return None
-        return int(1700 + (value * 200))
+        else:
+            return int(1700 + (value * 200)) #----> found a bug here 
 
-    def sig(self, value):
-        """
-        Custom mapping function for smooth and precise control over turning.
-        """
+    def sig(self, value): #Custom maaping function for more smoother and precise control over turning.
         if value < -1 or value > 1:
             return None
         elif value == 0:
@@ -127,10 +116,9 @@ class Control(Controller):
         else:
             return int((np.sign(value) * (27 ** (abs(value)) - 1) / (27 ** (1) - 1)) * 300 + 1500)
 
-
 #_____________________________________________________________Control-Thread__________________________________________________________________
 
-def run(control):       # Main Control Thread
+def run(control): #Main Control Thread
     # t = 0
     # t_prev = 0
 
@@ -155,12 +143,12 @@ def run(control):       # Main Control Thread
 
         if move & turn == 1500:
 
-            pi.set_servo_pulsewidth(control.THRUSTER_1, 1500)
-            pi.set_servo_pulsewidth(control.THRUSTER_2, 1500)
-            pi.set_servo_pulsewidth(control.THRUSTER_3, depth)
-            pi.set_servo_pulsewidth(control.THRUSTER_4, depth)
-
-        elif move != 1500:
+            pi.set_servo_pulsewidth(control.THRUSTER_1, 1500)   #### This Whole If-Else Statements
+            pi.set_servo_pulsewidth(control.THRUSTER_2, 1500)   #### can be replaced by a single
+            pi.set_servo_pulsewidth(control.THRUSTER_3, depth)  #### Matrix. Find that matrix, called 
+            pi.set_servo_pulsewidth(control.THRUSTER_4, depth)  #### Dynamic Matrix of the vehicle. 
+                                                                #### Use Matlab and control theory to
+        elif move != 1500:                                      #### achieve this. 
             pi.set_servo_pulsewidth(control.THRUSTER_1, move)
             pi.set_servo_pulsewidth(control.THRUSTER_2, move)
             pi.set_servo_pulsewidth(control.THRUSTER_3, depth)
@@ -175,10 +163,9 @@ def run(control):       # Main Control Thread
             # t_prev = t
         # time.sleep(0.01)
 
-
 #_______________________________________________________GUI-Thread________________________________________________________________________
 
-def GUI(control):       # GUI Thread --> sending data to BS
+def GUI(control): #GUI Thread --> sending data to BS
     print(GUI)
     time.sleep(2)
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -197,7 +184,7 @@ def GUI(control):       # GUI Thread --> sending data to BS
 
 
 if __name__ == '__main__':
-    control = Control(9, 11, 16, 8)     # --> GPIO Pin Values
+    control = Control(9, 11, 16, 8) #--> GPIO Pin Values
 
     control_thread = threading.Thread(target=run, args=(control,))
     gui_thread = threading.Thread(target=GUI, args=(control,))
